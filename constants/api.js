c32c6b702ff8ca14f9f718ac3dbbc939d0b88c78
jananisree-saitech <jananishree.saitech@gmail.com>
2025-09-05 10:59:55 +0530

    Frontend code push

---
+++ constants/api.js
@@ -0,0 +1,4 @@
+// export const API_URL = "https://wallet-api-cxqp.onrender.com/api";
+// export const API_URL = "http://localhost:5001/api";
+
+export const API_URL = "https://wallet-api-d4ve.onrender.com/api";  //my live server
